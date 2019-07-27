11c97c44c924d077bd1a2d39ed9c921913002e65
Malcolm Rodrigues <rmalcolm911@gmail.com>
2019-07-27 22:56:50 +0530

    Default select next Sunday

--- src/Components/DatePicker.js
+++ src/Components/DatePicker.js
@@ -5,8 +5,21 @@ class DatePicker extends Component{
     constructor(props) {
         super(props);
         this.onChange = this.onChange.bind(this);
-        this.date = this.props.date || new Date().toISOString().substring(0, 10);
+        this.date = this.props.date || this.nextSunday(new Date());
         this.props.updateDate(this.date);
+    }
+
+    nextSunday = (today) => {
+        let daysLeft = 7 - today.getDay();
+        if (daysLeft === 7) {
+            if (today.getHours() < 12) {
+                daysLeft = 0;
+            }
+        }
+        let nextSunday = new Date(today.getTime());
+        nextSunday.setTime(nextSunday.getTime() + daysLeft * 86400000);
+        console.log({ today, nextSunday });
+        return nextSunday.toISOString().substring(0, 10)
     }
     
     onChange = (e) => {
